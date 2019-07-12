f73d903d572a269690d968fcd8e454c6804d5e23
priyankasaxena11 <43996176+priyankasaxena11@users.noreply.github.com>
2019-07-12 13:15:20 +1000

    Update scan_activity.js

--- scan_activity.js
+++ scan_activity.js
@@ -1,4 +1,5 @@
 function startScanActivity(){
+    console.log("inside scanner");
     Cordova.exec(function success(result){
          document.getElementById("barcodeValue2").value = result;
      }, null, "PluginName", "new_activity", null);
