8075f6f9746e32333a68ac24cee784c53d3f7bd1
priyankasaxena11 <43996176+priyankasaxena11@users.noreply.github.com>
2019-08-01 12:18:16 +1000

    Update scan_activity.js

--- scan_activity.js
+++ scan_activity.js
@@ -1,8 +1,18 @@
 document.getElementById("scanBarcode2").addEventListener("click", startScanActivity);  
 
+function setFocusOnNext(elementName){
+    documant.getElementById(elementName).focus();    
+}
+
+function setBarcodeValue(){
+    var barcodeVal = document.getElementById("barcodeValueHidden").value;
+    var input_focused = document.activeElement === input && document.hasFocus();
+    documant.getElementById(input_focused).value = barcodeVal;
+}
+
 function startScanActivity(){
     console.log("inside scanner");
     Cordova.exec(function success(result){
-         document.getElementById("barcodeValue2").value = result;
+         document.getElementById("barcodeValueHidden").value = result;
      }, null, "PluginName", "new_activity", null);
 }
